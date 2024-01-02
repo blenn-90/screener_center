a1b0d51607a56ec5f343020c8df21b1a321a85ae
Alessandro Di Stefano <ads90.main@gmail.com>
2024-01-02 11:27:20 +0100

    first commit

---
+++ src/utilities/sources.py
@@ -0,0 +1,8 @@
+format_2decimal = "{:.2f}"
+format_4decimal = "{:.4f}"
+
+def fun_format_2decimal(value):
+    return format_2decimal.format(value)
+
+def fun_format_4decimal(value):
+    return format_4decimal.format(value)
